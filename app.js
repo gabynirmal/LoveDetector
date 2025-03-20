6de40921ef0db46ed9705febb6d67a815f4860db
Gabriela Nirmal <gabynirmal@icloud.com>
2025-03-20 00:21:49 -0400

    updated to use querySelector

--- app.js
+++ app.js
@@ -1,36 +1,40 @@
 function calculate() {
-    var p1 = document.getElementById("p1").value;
-    var p2 = document.getElementById("p2").value;
-    
-    var percentage = 0;
-    
-    if (p1.toLowerCase() == "gabriela nirmal" || p1.toLowerCase() == "gabriela" || p1.toLowerCase() == "gaby nirmal" || p1.toLowerCase() == "gaby") {
-      percentage = 100;
-      document.getElementById("percent").textContent = `${percentage}%`;
-    }
-    else if (p1.toLowerCase() == "computer" && p2.toLowerCase() == "science") {
-      percentage = 100;
-      document.getElementById("percent").textContent = `${percentage}%`;
-    }
-    else if (p1 == "" || p2 == "") {
-      percentage = "Person 1 or Person 2 is blank";
-      document.getElementById("percent").textContent = `${percentage}`;
-    }
-    else {
-       percentage = Math.floor(Math.random() * 100);
-       document.getElementById("percent").textContent = `${percentage}%`;
-    }
-    if (percentage <= 50) {
-      document.getElementById("heart").src = "images/broken_heart_love_detector.png";
-    }
-    else if (percentage > 50) {
-        document.getElementById("heart").src = "images/full_heart_love_detector.png";
-    }
-    else {
-        document.getElementById("heart").src = "images/question_heart_love_detector.png";
-    }
+  var p1 = document.querySelector("#p1").value;
+  var p2 = document.querySelector("#p2").value;
+  var percent = document.querySelector("#percent");
+
+  var newPercent;
+
+  if (
+    p1.toLowerCase() == "gabriela nirmal" ||
+    p1.toLowerCase() == "gabriela" ||
+    p1.toLowerCase() == "gaby nirmal" ||
+    p1.toLowerCase() == "gaby"
+  ) {
+    newPercent = 100;
+    percent.textContent = `${newPercent}%`;
+  } else if (p1.toLowerCase() == "computer" && p2.toLowerCase() == "science") {
+    newPercent = 100;
+    percent.textContent = `${newPercent}%`;
+  } else if (p1 == "" || p2 == "") {
+    newPercent = "Person 1 or Person 2 is blank";
+    percent.textContent = `${newPercent}`;
+  } else {
+    newPercent = Math.floor(Math.random() * 100);
+    percent.textContent = `${newPercent}%`;
   }
+  if (newPercent <= 50) {
+    document.querySelector("img").src = "images/broken_heart_love_detector.png";
+  } else if (newPercent > 50) {
+    document.querySelector("img").src = "images/full_heart_love_detector.png";
+  } else {
+    document.querySelector("img").src =
+      "images/question_heart_love_detector.png";
+  }
+}
+
+document.querySelector("button").addEventListener("click", calculate);
+
   
-  document.getElementById("button").addEventListener("click", calculate);
-  
+
   
